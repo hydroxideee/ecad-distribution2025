c80c2944a8568786d558c1353d420448f158dae2
anton <anton.zolochevskiy@gmail.com>
2025-10-23 12:54:40 +0100

    IMplement working solution and modify tester to just display

--- twotrafficlights/twotrafficlights.sv
+++ twotrafficlights/twotrafficlights.sv
@@ -5,6 +5,22 @@ module twotrafficlights(
       output logic [2:0] lightsB
     );
   logic [2:0] state;
-  // insert your code here
+  always_ff @( posedge clk or posedge rst ) begin
+    if (rst)
+        state <= 3'd0;
+      else 
+        state <= state + 1;
+  end
+  
+  logic flip;
+  logic [2:0] a;
+  logic [2:0] b;
+
+  always_comb begin
+    flip = state[2];
+    a = 3'b001;
+    b = {state[0] && !state[1], !state[0], !(state[0] ^ state[1])};
+    {lightsA, lightsB} = flip ? {b, a} : {a, b};
+  end
 
 endmodule
